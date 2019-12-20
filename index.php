4cb8ca6a144e1acbe7f6bf8bf22a08b4f610e5e1
axelgeoffroykouassi kouassi <ak659@afsconnect1.njit.edu>
2019-12-20 18:10:39 -0500

    Adding cases to display login, handle login verification, display questions, redirect to and display registration form, and display errors if any

--- index.php
+++ index.php
@@ -7,14 +7,72 @@ $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
-        $action = 'show_login';
+        $action = 'display_login';
     }
 }
 
+//Display login form login.php
 switch ($action) {
-    case 'show_login':
+    case 'display_login':
     {
         include('views/login.php');
         break;
     }
+
+    //Login credentials
+    case 'login': {
+    $email = filter_input(INPUT_POST, 'email_address');
+    $password = filter_input(INPUT_POST, 'password');
+
+    if ($email == NULL || $password == NULL) {
+        $error = "Email and Password not included.";
+        include('errors/error.php');
+    } else {
+        $userId = get_user($email, $password);
+        $firstName = return_fname($email, $password);
+        $lastName = return_lname($email, $password);
+        echo "User ID IS: $userId";
+        if ($userId == false) {
+            header("Location: .?action=display_registration");
+        } else {
+            header("Location: .?action=display_questions&userId=$userId&fname=$firstName&lname=$lastName");
+        }
+    }
+
+    break;
+}
+
+    case 'display_questions': {
+        $userId = filter_input(INPUT_GET, 'userId');
+        $firstName = filter_input(INPUT_GET, 'fname');
+        $lastName = filter_input(INPUT_GET, 'lname');
+        if ($userId == NULL || $userId < 0) {
+            header('Location: .?action=display_login');
+        } else {
+            $questions = get_user_questions($userId);
+            include('views/display_questions.php');
+        }
+        break;
+    }
+
+
+    case 'display_registration': {
+        include('views/registration.php');
+        break;
+    }
+
+
+
+
+    case 'display_question_form': {
+        $userId = filter_input(INPUT_GET, 'userId');
+        include('views/question_form.php');
+        break;
+    }
+
+    default: {
+        $error = 'Unknown Action';
+        include('errors/error.php');
+    }
+
 }
