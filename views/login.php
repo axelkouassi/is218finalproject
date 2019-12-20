c2ede4b7b77ea6e8aa324328b50f58ab57302d14
axelgeoffroykouassi kouassi <ak659@afsconnect1.njit.edu>
2019-12-20 16:41:56 -0500

    Creation of view of login form

--- views/login.php
+++ views/login.php
@@ -1,49 +1,8 @@
-<?php /*include('abstract-views/header.php'); */?>
-    <!DOCTYPE html>
-    <html lang="en-US">
-
-    <head>
-        <!--Bootstrap-->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-        <!--Favicon-->
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-        <link rel="manifest" href="favicon/site.webmanifest">
-
-        <!--Website Title-->
-        <title>IS 218 Project 3</title>
-
-        <!--Google Fonts-->
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,100' rel='stylesheet' type='text/css'>
-
-        <!--CSS-->
-        <link rel="stylesheet" href="./style.css" type="text/css" >
-
-    </head>
-
-    <body id="home_page">
-
-    //Top bar menu
-    <div class="navbar">
-        <nav id="nav_menu">
-            <a href="https://web.njit.edu/~ak659/index.html">
-                <img src="images/logo1.jpg" alt = "Axel Kouassi Personal Logo" id = "logo"></a>
-            <a href="registration.php"class ="right_align">Register</a>
-            <a href="login.php" class ="right_align">Login</a>
-        </nav>
-    </div>
-<!-- Main Container -->
-<main id="main_login">
+<?php include('abstract_views/header.php'); ?>
 
     <!-- Form for PHP -->
     <form action="index.php" method="post" id="login_form" class ="form">
-        <input type="hidden" name="action" value="validate_login">
+        <input type="hidden" name="action" value="login">
 
         <h1 id="login_header">Login Form</h1>
         <p style="color: red; ">* Required Fields</p><br>
@@ -67,11 +26,4 @@
 
     </form>
 </main>
-    </body>
-
-    <footer>
-        <p class="copyright">&copy; <?php echo date("Y");?> Axel Kouassi</p>
-    </footer>
-
-    </html>
-<?php /*include('abstract-views/footer.php.php'); */?>
+<?php include('abstract_views/footer.php'); ?>
