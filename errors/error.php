b324210851e6df1d8d767b41ee3b62f7b5be160c
axelgeoffroykouassi kouassi <ak659@afsconnect1.njit.edu>
2019-12-20 21:47:22 -0500

    fixing header and footer not displaying previously

--- errors/error.php
+++ errors/error.php
@@ -1,6 +1,6 @@
-<?php include '../views/abstract_views/header.php'; ?>
+<?php include 'views/abstract_views/header.php'; ?>
     <div id="main">
         <h1 class="top">Error</h1>
         <p class="first_paragraph"><?php echo $error; ?></p>
     </div>
-<?php include '../views/abstract_views/footer.php'; ?>
+<?php include 'views/abstract_views/footer.php'; ?>
